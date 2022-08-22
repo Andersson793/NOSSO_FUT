fac2aeb4865e9ba6d24f8f02e9bc4255707f9020
Andersson Gonçalves <anderssonsilva793@gmail.com>
2022-08-22 16:47:38 -0300

    key

--- src/components/axios.config.js
+++ src/components/axios.config.js
@@ -3,10 +3,12 @@ import {ref} from 'vue'
 
 //configuração e requisição
 
+const APIKey = 'Bearer test_663571e967d0ad3de7f53890f6d87d'
+
 const instance = axios.create({
     baseURL: 'https://api.api-futebol.com.br/v1/',
     headers: {
-        'Authorization':'Bearer test_663571e967d0ad3de7f53890f6d87d',
+        'Authorization': APIKey,
     }
 })
 
